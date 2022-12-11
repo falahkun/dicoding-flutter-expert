0a3a1ae83ccbbf54071f990e8c684a6eb09948c6
Falah Atharid Yusuf <30676438+falahkun@users.noreply.github.com>
2022-12-11 21:35:22 +0700

    Update get_percentage.sh

--- get_percentage.sh
+++ get_percentage.sh
@@ -46,24 +46,26 @@ runTests () {
 
 runReport() {
     if [ -f "coverage/lcov.info" ] && ! [ "$TRAVIS" ]; then
-        set -e
-PATH_COVERAGE=$1
-MIN_COVERAGE_PERC=80
+        PATH_COVERAGE=$1
+        MIN_COVERAGE_PERC=80
 
-percentageRate=$(lcov --summary "$PATH_COVERAGE" | grep "lines......" | cut -d ' ' -f 4 | cut -d '%' -f 1)
+        percentageRate=$(lcov --summary "$PATH_COVERAGE" | grep "lines......" | cut -d ' ' -f 4 | cut -d '%' -f 1)
+        
+        echo percentageRate
+        pwd
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
+        RED='\033[0;31m'
+        GREEN='\033[0;32m'
 
-if [ "$(echo "${percentageRate} < $MIN_COVERAGE_PERC" | bc)" -eq 1 ]; then
-    printf "${RED}Error: Your coverage rate is to low, expected ${MIN_COVERAGE_PERC}% but have ${percentageRate}%.\n"
-    printf "${RED}Please add more tests to cover your code.\n"
-    printf "${RED}To see in local your coverage rate use:\n"
-    printf "${RED}    sh scripts/create_clean_lcov_and_generate_html.sh true\n"
-    exit 1
-else
-    printf "${GREEN}Coverage rate is fine 👍. Build continue...\n"
-fi
+        if [ "$(echo "${percentageRate} < $MIN_COVERAGE_PERC" | bc)" -eq 1 ]; then
+            printf "${RED}Error: Your coverage rate is to low, expected ${MIN_COVERAGE_PERC}% but have ${percentageRate}%.\n"
+            printf "${RED}Please add more tests to cover your code.\n"
+            printf "${RED}To see in local your coverage rate use:\n"
+            printf "${RED}    sh scripts/create_clean_lcov_and_generate_html.sh true\n"
+            exit 1
+        else
+            printf "${GREEN}Coverage rate is fine 👍. Build continue...\n"
+        fi
     fi
 }
 
